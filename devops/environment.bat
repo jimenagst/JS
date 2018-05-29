bad2eacb7eda60b57af8b92b95f8081ca446a372
Jimena Sanabria <jsanabria@pros.com>
2018-05-29 13:51:52 -0400

    Updating files to set variables for vm and server name

--- devops/environment.bat
+++ devops/environment.bat
@@ -1,4 +1,8 @@
-@echo off
+
+@echo off 
+
+REM Update OS_USERNAME and OS_PASSWORD with your data before executing this file
+
 set OS_AUTH_URL=http://10.28.3.240:5000/v3
 set OS_IDENTITY_API_VERSION=3
 set OS_TENANT_NAME=51691ab150af40c8a7be5ee425989549
@@ -10,3 +14,6 @@ set OS_FLAVOR=f1.desktop.large
 set OS_IMAGE=ubuntu-16.04-amd64-server_17052018
 set OS_KEY_PAIR_NAME=jimena
 set OS_REGION_NAME=bootstack-jalasoft
+set VM_NAME=devops-vm-1
+set SERVER_NAME=devops-vm-1
+set FLAVOR_NAME=f2.desktop.medium
